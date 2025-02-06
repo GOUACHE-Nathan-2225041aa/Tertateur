a6d7f8cb00992412d36b6da41d6d95902c431961
suricate <nathan.gouache@etu.univ-amu.fr>
2025-02-06 15:57:18 +0100

    Bon bah finalement on fait une api (j'ai pas la vision)

--- src/Entity/Photo.php
+++ src/Entity/Photo.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\PhotoRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PhotoRepository::class)]
+#[ApiResource]
 class Photo
 {
     #[ORM\Id]
